3829f6b0fc3188f72f92d79d6c2a089813563079
derin7ler <derin.sevenler@gmail.com>
2014-04-07 15:11:14 -0400

    sub-pixel alignment
    Now images are aligned with sub-pixel accuracy. This only works for
    images separated by at least 1 pixel (roughly).

--- Alignment and matching/phCorrAlign.m
+++ Alignment and matching/phCorrAlign.m
@@ -5,7 +5,6 @@
 function [delta,q] = phCorrAlign(I1, I2)
 % delta is the [row, column] displacement of I2 relative to I1.
 
-block_r=0;
 block_outer = 300;
 
 % Take FFT of each image
@@ -18,19 +17,25 @@ pdm = exp(1i*(angle(F1)-angle(F2)));
 pcf = real(ifft2(pdm));
 pcf = fftshift(pcf);
 center = floor(size(pcf)/2)+1;
-% figure; imagesc(pcf);
-% colorbar;
-% rr = (center(1)-block_r):(center(1)+block_r);
-% cc = (center(2)-block_r):(center(2)+block_r);
 
 pcf(center(1), center(2)) = 0;
+v = pcf((center(1)-1):(center(1)+1), (center(2)-1):(center(2)+1));
+
+pcf(center(1), center(2)) = sum(v(:))./8;
+
 [r,c] = meshgrid(1:size(pcf,1), 1:size(pcf,2));
 mask = sqrt((r-size(pcf,1)/2).^2 + (c-size(pcf,2)/2).^2) > block_outer;
 pcf(mask) = 0;
 
-% figure; imagesc(pcf);
-% colorbar;
+figure; imagesc(pcf);
+colorbar;
 [q, idx] = max(pcf(:));
 [r, c] = ind2sub(size(pcf),idx);
 v = [r c];
+delta = v - center;
+
+roi = pcf(r-5:r+5, c-5:c+5);
+P = peakfit2d(roi);
+
+v = P - (floor(size(roi)/2) + 1) + [r c];
 delta = v - center;
